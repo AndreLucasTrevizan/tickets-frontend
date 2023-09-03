88c0d216892f88de093cf679c0591a52d8541642
André Lucas Trevizan <trevizan.al@gmail.com>
2023-09-03 18:23:14 -0300

    Project created

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Tickets - Sign In</title>
+            </Head>
+
+            <body>
+                <h1>Hello World</h1>
+            </body>
+        </>
+    );
+}
